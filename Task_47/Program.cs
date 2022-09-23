befa7a507e3133a443c01c51f9ebb438eb5e5981
unknown <nastena-alhovik@yandex.ru>
2022-09-23 21:24:16 +0300

    add Task 47 HomeWork 7

---
+++ Task_47/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+
+Console.Write("m = ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("n = ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+double[,] matrix = new double[m, n];
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        int s = new Random().Next(-1, 1);
+        if (s == 0) { s = 1; }
+        matrix[i, j] = Math.Round(new Random().NextDouble() * 10 * s, 1);
+        Console.Write($" {matrix[i, j]} ");
+    }
+    Console.WriteLine();
+}
