e979e355a3e9489f581a3bba27b843b5c20cd067
unknown <nastena-alhovik@yandex.ru>
2022-09-24 22:03:08 +0300

    add Task_50 HomeWork 7

--- Task_50/Program.cs
+++ Task_50/Program.cs
@@ -1,52 +1,24 @@
 ﻿Console.Clear();
 
-int[,] FillMatrix(int m, int n)
+int[,] matrix = new int[3, 4];
+for (int i = 0; i < matrix.GetLength(0); i++)
 {
-    int[,] matrix = new int[m, n];
-    for (int i = 0; i < matrix.GetLength(0); i++)
+    for (int j = 0; j < matrix.GetLength(1); j++)
     {
-        for (int j = 0; j < matrix.GetLength(1); j++)
-        {
-            matrix[i, j] = new Random().Next(0, 10);
-            Console.Write($" {matrix[i, j]} ");
-        }
-        Console.WriteLine();
+        matrix[i, j] = new Random().Next(0, 10);
+        Console.Write($" {matrix[i, j]} ");
     }
-    return matrix;
+    Console.WriteLine();
 }
-
-void NumberPosition(int[,] array)
+Console.Write("Укажите строку (x) = ");
+int x = int.Parse(Console.ReadLine());
+Console.Write("Укажите столбец (y) = ");
+int y = int.Parse(Console.ReadLine());
+if (x < 0 | x > matrix.GetLength(0) - 1 | y < 0 | y > matrix.GetLength(1) - 1)
 {
-    Console.Write("Введите число: ");
-    int number = int.Parse(Console.ReadLine());
-    int x = 0;
-    int y = 0;
-    int count = 0;
-
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            if (number == array[i, j])
-            {
-                x = i;
-                y = j;
-                Console.WriteLine($"Позиция элемента в массиве [{x},{y}]");
-            }
-            else
-            {
-                count++;
-            }
-            if (count == array.GetLength(0) * array.GetLength(1))
-            {
-                System.Console.WriteLine("Такого числа в массиве нет");
-            }
-
-        }
-    }
+    Console.WriteLine("Такого числа в массиве нет");
 }
-
-
-
-int[,] array = FillMatrix(4, 3);
-NumberPosition(array);
+else
+{
+    Console.WriteLine("Значение элемента массива: " + matrix[x, y]);
+}
